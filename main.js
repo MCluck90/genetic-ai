eadc737d37dd9e400264ba20cb6e12fd00417208
Mike C <MCluck90@gmail.com>
2015-11-12 19:41:47 -0700

    Setup scaffolding and added yargs

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// Main entry point
+let population = [];
+let FITNESS_THRESHOLD = 0;
+let maxFitness = Number.MIN_SAFE_INTEGER;
+
+while (maxFitness < FITNESS_THRESHOLD) {
+  population = select(population, maxFitness);
+  crossover(population, maxFitness);
+  mutate(population, maxFitness);
+  maxFitness = Number.MIN_VALUE;
+  population.forEach(function(hypothesis) {
+    hypothesis.fitness = evaluate(hypothesis);
+    if (hypothesis.fitness > maxFitness) {
+      maxFitness = hypothesis.fitness;
+    }
+  });
+}
