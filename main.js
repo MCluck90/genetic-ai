639e96593ad5ee7056f6b87f24a52a981702f4fb
Mike C <MCluck90@gmail.com>
2015-11-12 19:54:36 -0700

    Added command line arguments support and bash support

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+'use strict';
+
+let yargs = require('yargs').argv;
 let population = [];
-let FITNESS_THRESHOLD = 0;
+const FITNESS_THRESHOLD = 0;
 let maxFitness = Number.MIN_SAFE_INTEGER;
+let selectKey = yargs.select || 'fitness';
+let crossoverKey = yargs.crossover || 'single';
+let mutationRate = yargs.mutation || 0.05;
+let evaluateKey = yargs.evaluate || 'ascii';
+let select = require(`./${selectKey}-select.js`);
+let crossover = require(`./${crossoverKey}-crossover.js`);
+let evaluate = require(`./${evaluateKey}-evaluate.js`);
 
 while (maxFitness < FITNESS_THRESHOLD) {
   population = select(population, maxFitness);
