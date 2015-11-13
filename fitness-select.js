2fe265bd0c9212e7fc73c68fb17607574a0f08c7
JonHolt <jonathanholt92@gmail.com>
2015-11-13 11:58:39 -0700

    fitness select now usually selects between 25 and 50 of the population based on relative fitness.

---
+++ fitness-select.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let fitness = function(population, maxFitness){
+    let newPop = [];
+    population.forEach(function(hypothesis){
+        let chance = (hypothesis.fitness === 0)? 1 : maxFitness / hypothesis.fitness;
+        if(Math.random() / 2 < chance){
+            newPop.push(hypothesis);
+        }
+    });
+    return newPop;
+}
+
+module.exports = fitness;
